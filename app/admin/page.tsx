9c960ee098e858f9506fe3c3fbdfd4f2b9fed4b0
Hung Nguyen <nd.hung@me.com>
2024-08-20 21:09:35 +0200

    chore: Update logo and appointment cards layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -13,13 +13,7 @@ const AdminPage = async () => {
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
         <Link href="/" className="cursor-pointer">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={32}
-            width={162}
-            alt="logo"
-            className="h-8 w-fit"
-          />
+          <Image src="/assets/icons/logo-full.svg" height={32} width={162} alt="logo" className="h-8 w-fit" />
         </Link>
 
         <p className="text-16-semibold">Admin Dashboard</p>
@@ -28,9 +22,7 @@ const AdminPage = async () => {
       <main className="admin-main">
         <section className="w-full space-y-4">
           <h1 className="header">Welcome 👋</h1>
-          <p className="text-dark-700">
-            Start the day with managing new appointments
-          </p>
+          <p className="text-dark-700">Start the day with managing new appointments</p>
         </section>
 
         <section className="admin-stat">
@@ -40,12 +32,7 @@ const AdminPage = async () => {
             label="Scheduled appointments"
             icon={"/assets/icons/appointments.svg"}
           />
-          <StatCard
-            type="pending"
-            count={appointments.pendingCount}
-            label="Pending appointments"
-            icon={"/assets/icons/pending.svg"}
-          />
+          <StatCard type="pending" count={appointments.pendingCount} label="Pending appointments" icon={"/assets/icons/pending.svg"} />
           <StatCard
             type="cancelled"
             count={appointments.cancelledCount}
